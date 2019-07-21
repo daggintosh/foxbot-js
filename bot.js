60500643c32995d9fd9149fc5324805036ad3002
Dagg <theamazingdoge91@gmail.com>
2019-07-21 12:15:27 -0700

    Finished rewrite of bot

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const randomcolour = require('randomcolor')
 
 var token = "EDIT.TOKEN"
 
@@ -16,22 +17,54 @@ const foxPhrases = [
     "Wow! A fox!"
 ]
 
-client.login(token);
+client.login(token)
 
 client.on('ready', () => {
-    console.log('Ready!');
+    console.log('Ready!')
 })
 
 client.on('message', msg => {
     switch(msg.content)
     {
+        case "/about":
+            var aboutEmbed = new Discord.RichEmbed()
+            .setColor(randomcolour())
+            .setThumbnail("https://dagg.xyz/randomfox/images/" + Math.floor(Math.random() * 126) + ".jpg")
+            .setTitle("GitHub")
+            .setURL("https://github.com/daggintosh/foxbot-js")
+            .setDescription("**Hello!**")
+            .setAuthor("FoxBot", "https://cdn.discordapp.com/avatars/601967284394917900/f25955e890f89f1015762647f82ea555.webp")
+            msg.channel.send(aboutEmbed)
+            break;
         case "/fox":
+            fox()
+            function fox()
+            {
             var foxEmbed = new Discord.RichEmbed()
-            .setColor(Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),Math.floor(Math.random() * 256))
+            .setColor(randomcolour())
             .setTitle(foxPhrases[Math.floor(Math.random()*foxPhrases.length)])
             .setAuthor(msg.author.username, msg.author.avatarURL)
             .setImage("https://dagg.xyz/randomfox/images/" + Math.floor(Math.random() * 126) + ".jpg")
-            msg.channel.send(foxEmbed);
-            break;
+            let filterplay = (reaction, user) => reaction.emoji.name === "➡" && user.id === msg.author.id
+            let filterstop = (reaction, user) => reaction.emoji.name === "⏹" && user.id === msg.author.id
+            msg.channel.send(foxEmbed)
+            .then(function(msg){
+                let collectorplay = msg.createReactionCollector(filterplay, { time: 60000 })
+                let collectorstop = msg.createReactionCollector(filterstop, { time: 60000 })
+                collectorplay.on('collect', z => {
+                    msg.delete();
+                    fox();
+                })
+                collectorstop.on('collect', z => {
+                    msg.delete();
+                })
+                msg.react("➡")
+                .then(z =>{
+                    msg.react("⏹")
+                })
+            })     
+            }
+            break
     }
 })
+
