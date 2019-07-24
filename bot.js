fe089a94ed746cfe7d75d6e4517a13a351953820
Dagg <theamazingdoge91@gmail.com>
2019-07-24 02:16:12 -0700

    Tweaks

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const randomcolour = require('randomcolor')
+const youtube = require('ytdl-core')
+const request = require('request');
 
-//#region Bot Token
-var token = "EDIT.TOKEN"
-//#endregion
+const token = require("./token.json"); // BOT TOKEN
+const config = require("./config.json") // CONFIG
 
 const foxPhrases = [
     "A fox appears!", 
@@ -19,74 +20,268 @@ const foxPhrases = [
     "Wow! A fox!"
 ]
 
-client.login(token)
+const catPhrases = [
+    "A cat appears!", 
+    "A cat is here!", 
+    "Theres a cat here!", 
+    "A cat has manifested!",
+    "A cat has taken hold!",
+    "There's a cat in my boot!",
+    "A wild cat has appeared!",
+    "A cat challenges you!",
+    "You see a cat!",
+    "Wow! A cat!"
+]
+
+const dogPhrases = [
+    "A dog appears!", 
+    "A dog is here!", 
+    "Theres a dog here!", 
+    "A dog has manifested!",
+    "A dog has taken hold!",
+    "There's a dog in my boot!",
+    "A wild dog has appeared!",
+    "A dog challenges you!",
+    "You see a dog!",
+    "Wow! A dog!"
+]
+
+client.login(token.token)
 
 client.on('ready', () => {
     console.log('Ready!')
-    client.user.setActivity("Foxes in " + client.guilds.size + " Guilds", { type: 'LISTENING' })
+    client.user.setActivity("foxes in " + client.guilds.size + " guilds", { type: 'LISTENING' })
 })
 
+let voiceActive = false
+
 client.on('message', msg => {
-    switch(msg.content)
+    if(msg.content.indexOf(config.prefix) !== 0) return
+    const filter = (reaction, user) => reaction.emoji.name === "➡" && user.id === msg.author.id || reaction.emoji.name === "⏹" && user.id === msg.author.id
+    const argument = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = argument.shift().toLowerCase()
+
+    switch(command)
     {
-        //#region /about
-        case "/about":
-            var aboutEmbed = new Discord.RichEmbed()
+        //#region about
+        case "about":
+            let aboutEmbed = new Discord.RichEmbed()
             .setColor(randomcolour())
             .setThumbnail("https://dagg.xyz/randomfox/images/" + Math.floor(Math.random() * 125) + ".jpg")
             .setTitle("GitHub")
             .setURL("https://github.com/daggintosh/foxbot-js")
             .setDescription("**Hello!**")
             .setAuthor("FoxBot", "https://cdn.discordapp.com/avatars/601967284394917900/f25955e890f89f1015762647f82ea555.webp")
+            .setFooter(Date())
             msg.channel.send(aboutEmbed)
             break;
         //#endregion
 
-        //#region /fox
-        case "/fox":
-            if (msg.guild.me.hasPermission("MANAGE_MESSAGES"))
-            {
-                msg.delete()
-            }
+        //#region fox
+        case "fox":
             fox()
             function fox()
             {
-                var foxEmbed = new Discord.RichEmbed()
+                let foxEmbed = new Discord.RichEmbed()
                 .setColor(randomcolour())
                 .setTitle(foxPhrases[Math.floor(Math.random()*foxPhrases.length)])
                 .setAuthor(msg.author.username, msg.author.avatarURL)
                 .setImage("https://dagg.xyz/randomfox/images/" + Math.floor(Math.random() * 125) + ".jpg")
-                let filterplay = (reaction, user) => reaction.emoji.name === "➡" && user.id === msg.author.id
-                let filterstop = (reaction, user) => reaction.emoji.name === "⏹" && user.id === msg.author.id
+                .setFooter(Date())
                 msg.channel.send(foxEmbed)
-                .then(function(msg){
-                    let collectorplay = msg.createReactionCollector(filterplay, { time: 60000 })
-                    let collectorstop = msg.createReactionCollector(filterstop, { time: 60000 })
-                    collectorplay.on('collect', z => {
-                        msg.delete();
-                        fox();
-                    })
-                    collectorstop.on('collect', z => {
-                        msg.delete();
+                .then(msg => {
+                    msg.createReactionCollector(filter , { time: 60000 })
+                    .on('collect', reaction => {
+                        switch(reaction.emoji.name)
+                        {
+                            case "➡":
+                                msg.delete()
+                                fox()
+                                break
+                            case "⏹":
+                                msg.delete()
+                                break
+                        }
                     })
                     msg.react("➡")
                     .then(z =>{
                         msg.react("⏹")
                     })
-                })     
+                }) 
+            }
+            break
+        //#endregion
+
+        //#region cat
+        case "cat":
+            cat()
+            function cat()
+            {
+                request('http://aws.random.cat/meow', { json: true } , (error, response, body) => {   
+                    let catEmbed = new Discord.RichEmbed()      
+                    .setColor(randomcolour())
+                    .setTitle(catPhrases[Math.floor(Math.random()*catPhrases.length)])
+                    .setAuthor(msg.author.username, msg.author.avatarURL)
+                    .setImage(body.file)
+                    .setFooter(Date())
+                    msg.channel.send(catEmbed)
+                    .then(msg => {
+                        msg.createReactionCollector(filter , { time: 60000 })
+                        .on('collect', reaction => {
+                            switch(reaction.emoji.name)
+                            {
+                                case "➡":
+                                    msg.delete()
+                                    cat()
+                                    break
+                                case "⏹":
+                                    msg.delete()
+                                    break
+                            }
+                        })
+                        msg.react("➡")
+                        .then(z =>{
+                            msg.react("⏹")
+                        })
+                    })
+                })
             }
             break
         //#endregion
         
-        //#region /time
-        case "/time":
-            var timeEmbed = new Discord.RichEmbed()
+        //#region dog
+        case "dog":
+            dog()
+            function dog()
+            {
+                request('https://dog.ceo/api/breeds/image/random', { json: true} , (error, response, body) => { 
+                    let dogEmbed = new Discord.RichEmbed()
+                    .setColor(randomcolour())
+                    .setTitle(dogPhrases[Math.floor(Math.random()*dogPhrases.length)])
+                    .setAuthor(msg.author.username, msg.author.avatarURL)
+                    .setImage(body.message)
+                    .setFooter(Date())
+                    msg.channel.send(dogEmbed)
+                    .then(msg => {
+                        msg.createReactionCollector(filter , { time: 60000 })
+                        .on('collect', reaction => {
+                            switch(reaction.emoji.name)
+                            {
+                                case "➡":
+                                    msg.delete()
+                                    dog()
+                                    break
+                                case "⏹":
+                                    msg.delete()
+                                    break
+                            }
+                        })
+                        msg.react("➡")
+                        .then(z =>{
+                            msg.react("⏹")
+                        })
+                    })
+                })
+            }
+            break
+        //#endregion
+
+        //#region time
+        case "time":
+            let timeEmbed = new Discord.RichEmbed()
             .setColor(randomcolour())
             .setDescription("**It's time to go to bed, you dolt.**")
+            .setFooter(Date())
             .setAuthor(msg.author.username, msg.author.avatarURL)
             msg.channel.send(timeEmbed)
             break
         //#endregion
+
+        //#region ping
+        case "ping":
+            var pingMil = client.ping
+            var pingColour = 0
+            if(pingMil >= 200)
+            {
+                pingColour = "ff0000"
+            }
+            else if(pingMil >= 150)
+            {
+                pingColour = "f6ff00"
+            }
+            else if(pingMil < 150)
+            {
+                pingColour = "00ffe5"
+            }
+            let pingEmbed = new Discord.RichEmbed()
+            .setColor(pingColour)
+            .setDescription("Ping: " + "**" + Math.floor(pingMil) + "** milliseconds")
+            .setFooter(Date())
+            .setAuthor(msg.author.username, msg.author.avatarURL)
+            msg.channel.send(pingEmbed)
+            break
+        //#endregion
+
+        //#region help
+        case "help":
+            let helpEmbed = new Discord.RichEmbed()
+            .setColor(randomcolour())
+            .addField(config.prefix + "help", "Displays this screen", true)
+            .addField(config.prefix + "about", "About the bot", true)
+            .addField(config.prefix + "ping", "Pong!", true)
+            .addField(config.prefix + "time", "Tells the time", true)
+            .addField(config.prefix + "fox", "Post a random fox", true)
+            .addField(config.prefix + "cat", "Post a random cat", true)
+            .addField(config.prefix + "dog", "Post a random dog", true)
+            .addField(config.prefix + "play [YouTube URL]", "Plays a song", true)
+            .setFooter(Date())
+            .setAuthor(msg.author.username, msg.author.avatarURL)
+            .setThumbnail("https://dagg.xyz/randomfox/images/" + Math.floor(Math.random() * 125) + ".jpg")
+            msg.channel.send(helpEmbed)
+            break
+        //#endregion
+
+        //#region play
+        case "play":
+            if (argument[0] == undefined){ msg.reply ("Do you want me to just scream?") }
+            else if(voiceActive == true) { msg.reply("I'm already playing something!") }
+            else if (msg.member.voiceChannel == undefined) { msg.reply("You aren't in a voice channel!") }
+            else if(argument[0].includes("youtube.com/watch?v=") || argument[0].includes("youtu.be/")){
+                let voiceChannel = msg.member.voiceChannel;
+                let url = argument[0]
+                let video = youtube(url)
+                youtube.getInfo(url, (error, info) => {
+                    voiceChannel.join()
+                    .then(connection => {
+                        voiceActive = true;
+                        let dispatch = connection.playStream(video)
+                        dispatch.setVolume(0.5)
+                        let ytEmbed = new Discord.RichEmbed()
+                        .setAuthor(msg.author.username, msg.author.avatarURL)
+                        .setFooter(Date())
+                        .addField("**Now Playing**", info.title)
+                        .setTitle(info.video_url)
+                        .setURL(info.video_url)
+                        .setColor(randomcolour())
+                        msg.channel.send(ytEmbed)
+                        .then(msg => {
+                            dispatch.on('end', z => { voiceActive = false, connection.dispatcher.end(), voiceChannel.leave(), msg.delete()})
+                            msg.createReactionCollector(filter , { time: null })
+                            .on('collect', reaction => {
+                                switch(reaction.emoji.name)
+                                {
+                                    case "⏹":
+                                        voiceChannel.leave()
+                                        break
+                                }
+                            })
+                            msg.react("⏹")
+                        })
+                    })
+                })
+            }
+            else { msg.reply("Invalid URL")}
+            break
+        //#endregion
     }
 })
-
